aba4921602782902c95050cf520fde69dade96c2
Bruce Trindade <brucetrindade2000@gmail.com>
2021-05-03 18:05:24 -0300

    Telas Adicionadas

--- app/src/main/java/com/example/listadetarefasapp/MainActivity.java
+++ app/src/main/java/com/example/listadetarefasapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.listadetarefasapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.listadetarefasapp.R;
+import com.example.listadetarefasapp.TelaAddTarefaActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends TelaAddTarefaActivity {
+
+    FloatingActionButton butao_add;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        butao_add = findViewById(R.id.butao_add_tarefa);
+        butao_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent adicionarTarefa = new Intent(MainActivity.this, TelaAddTarefaActivity.class);
+                startActivity(adicionarTarefa);
+            }
+
+
+
+
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/listadetarefasapp/TelaAddTarefaActivity.java
@@ -0,0 +1,14 @@
+package com.example.listadetarefasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaAddTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_add_tarefa);
+    }
+}
